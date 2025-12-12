cd6abd808a16276e6a7e0c2b059a044de7f2fcd6
rubinks24 <rubinrajkumar26@gmail.com>
2025-12-12 14:27:23 +0530

    if else

--- DAY 3/q4.java
+++ DAY 3/q4.java
@@ -1,13 +1,15 @@
 import java.util.Scanner;
-public class q1 {
+public class q4 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         int b = sc.nextInt();
         
-        if(a > b)
-            System.out.println(a + " is greatest");
+        int diff = a - b;
+        
+        if(diff % 2 == 0)
+            System.out.println("Difference is Even");
         else
-            System.out.println(b + " is greatest");
+            System.out.println("Difference is Odd");
     }
 }
--- DAY 3/q6.java
+++ DAY 3/q6.java
@@ -1,13 +1,16 @@
 import java.util.Scanner;
-public class q1 {
+public class q6 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         int b = sc.nextInt();
+        int c = sc.nextInt();
         
-        if(a > b)
+        if(a >= b && a >= c)
             System.out.println(a + " is greatest");
-        else
+        else if(b >= a && b >= c)
             System.out.println(b + " is greatest");
+        else
+            System.out.println(c + " is greatest");
     }
 }
