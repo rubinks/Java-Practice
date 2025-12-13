cb119cfa47f93aafe2f2c3c78c75a9a4b9652b80
rubinks24 <rubinrajkumar26@gmail.com>
2025-12-13 12:13:16 +0530

    day 3

---
+++ DAY 2/qs17.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class qs17 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        if (n > 0 && (n & (n - 1)) == 0) {
+            System.out.println("Power of 2");
+        } else {
+            System.out.println("Not a power of 2");
+        }
+    }
+}
